c32e44c2c67be779e51dc4600faab447be2810b9
ACV777 <cherianadrian54@gmail.com>
2026-09-06 11:42:49 +0530

    Update Challenge 5 to FutureTech University Fake Certificate Problem Case Study with Option A as correct answer

--- test_arena.py
+++ test_arena.py
@@ -108,10 +108,10 @@ class Round2ArenaTestCase(unittest.TestCase):
         d4 = json.loads(res4.data)
         self.assertTrue(d4['is_correct'])
 
-        # 5. Challenge 5 (Case Study: "B")
+        # 5. Challenge 5 (Case Study: "A")
         res5 = self.client.post('/api/submit-challenge', json={
             'challenge_id': 5,
-            'answer': 'B'
+            'answer': 'A'
         })
         d5 = json.loads(res5.data)
         self.assertTrue(d5['is_correct'])
